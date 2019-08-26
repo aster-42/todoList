e12b304793a97525dc9c537188e2a355ba9b320b
3Fx <31286515+ruaco@users.noreply.github.com>
2019-08-26 11:10:45 +0800

    Update TodoService.java
    clear import

--- src/main/java/com/axe/todo/controller/TodoService.java
+++ src/main/java/com/axe/todo/controller/TodoService.java
@@ -5,10 +5,8 @@ import com.axe.todo.entity.TodoEnum;
 import com.axe.todo.model.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
 
 @Controller
 public class TodoService {
